ecbc4dc5b2c6f83ec05be6c8d32f4e172de2eea2
Natalia Klimareva <60422598+MIA1kl@users.noreply.github.com>
2020-02-04 14:51:42 +0600

    Multiple classes

---
+++ OtherClass.java
@@ -0,0 +1,9 @@
+package com.company;
+
+class OtherClass {
+    public static void main(String[] args) {
+        Car myCar = new Car();     // Create a myCar object
+        myCar.fullThrottle();      // Call the fullThrottle() method
+        myCar.speed(200);          // Call the speed() method
+    }
+}
